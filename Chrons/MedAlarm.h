acb799a5daad01b8055c1f6fd48375fcae18d797
Jesse <tartarus16@gmail.com>
2014-05-06 02:29:23 -0400

    added med alarm

---
+++ Chrons/MedAlarm.h
@@ -0,0 +1,13 @@
+//
+//  MedAlarm.h
+//  Chrons
+//
+//  Created by Jesse Lifshitz on 5/6/14.
+//  Copyright (c) 2014 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MedAlarm : UIViewController
+
+@end
